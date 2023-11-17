d9841a2a86ff11cf078fa426c0bcb9cbe7040184
Ricardo Tun <me@ricardotun.mx>
2023-11-17 02:30:55 -0500

    [ruby] assembly line: add solution

--- ruby/assembly-line/assembly_line.rb
+++ ruby/assembly-line/assembly_line.rb
@@ -1,13 +1,27 @@
 class AssemblyLine
+  CARS_PER_HOUR = 221
+
   def initialize(speed)
     @speed = speed
   end
 
   def production_rate_per_hour
-    raise 'Please implement the AssemblyLine#production_rate_per_hour method'
+    if @speed.between?(0, 4)
+      success_rate = 1.0
+    elsif @speed.between?(5, 8)
+      success_rate = 0.9
+    elsif @speed == 9 
+      success_rate = 0.8
+    elsif @speed == 10
+      success_rate = 0.77
+    else 
+      raise "The assembly line's speed can range from 0 (off) to 10 (maximum)."
+    end
+
+    @speed * success_rate * CARS_PER_HOUR
   end
 
   def working_items_per_minute
-    raise 'Please implement the AssemblyLine#working_items_per_minute method'
+    (production_rate_per_hour / 60).floor
   end
 end
