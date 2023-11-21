984a1cd8ea0cd34d1188d3d8e642fb73f895730b
Ricardo Tun <me@ricardotun.mx>
2023-11-21 18:38:35 -0500

    [ruby] savings account: add solution

--- ruby/savings-account/savings_account.rb
+++ ruby/savings-account/savings_account.rb
@@ -1,13 +1,28 @@
 module SavingsAccount
   def self.interest_rate(balance)
-    raise 'Please implement the SavingsAccount.interest_rate method'
+    if balance.negative?
+      3.213
+    elsif balance < 1_000
+      0.500
+    elsif balance < 5_000
+      1.621
+    else
+      2.475
+    end
   end
 
   def self.annual_balance_update(balance)
-    raise 'Please implement the SavingsAccount.annual_balance_update method'
+    balance * (1 + interest_rate(balance)/100.0)
   end
 
   def self.years_before_desired_balance(current_balance, desired_balance)
-    raise 'Please implement the SavingsAccount.years_before_desired_balance method'
+    years = 0
+
+    while current_balance < desired_balance
+      current_balance = annual_balance_update(current_balance)
+      years += 1
+    end
+
+    years
   end
 end
