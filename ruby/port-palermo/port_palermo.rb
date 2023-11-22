8d11e49c74694d8f400cf2675aa41e4acd781a17
Ricardo Tun <me@ricardotun.mx>
2023-11-21 20:42:02 -0500

    [ruby] port of palermo: add solution 2

--- ruby/port-palermo/port_palermo.rb
+++ ruby/port-palermo/port_palermo.rb
@@ -2,16 +2,12 @@ module Port
   IDENTIFIER = :PALE
 
   def self.get_identifier(city)
-    city.slice(0,4).to_sym.upcase
+    city[0, 4].upcase.to_sym
   end
 
   def self.get_terminal(ship_identifier)
-    ship = ship_identifier.slice(0,3).upcase
+    ship_id = ship_identifier[0,3].upcase
 
-    if ship == "OIL" || ship == "GAS"
-      :A
-    else
-      :B
-    end
+    ship_id == "OIL" || ship_id == "GAS" ? :A : :B
   end
 end
