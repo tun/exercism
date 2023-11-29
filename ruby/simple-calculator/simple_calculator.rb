2bfbe50a1f8fb5ee6bd83208a1871a6770bda4af
Ricardo Tun <me@ricardotun.mx>
2023-11-29 06:50:04 -0500

    [ruby] simple calculator: add solution

--- ruby/simple-calculator/simple_calculator.rb
+++ ruby/simple-calculator/simple_calculator.rb
@@ -1,7 +1,28 @@
 class SimpleCalculator
   ALLOWED_OPERATIONS = ['+', '/', '*'].freeze
 
+  class UnsupportedOperation < StandardError
+  end
+
   def self.calculate(first_operand, second_operand, operation)
-    raise 'Please implement the SimpleCalculator.calculate method'
+    raise ArgumentError unless first_operand.is_a?(Integer)
+    raise ArgumentError unless second_operand.is_a?(Integer) 
+    raise UnsupportedOperation unless ALLOWED_OPERATIONS.include? operation
+
+    begin
+      result = case operation
+               when '+'
+                 first_operand + second_operand
+               when '*'
+                 first_operand * second_operand
+               when '/'
+                 first_operand / second_operand
+               end
+
+    "#{first_operand} #{operation} #{second_operand} = #{result}"
+
+    rescue ZeroDivisionError
+      'Division by zero is not allowed.'
+    end
   end
 end
