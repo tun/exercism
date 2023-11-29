07e97bfa075febd6106c27a20e9a4acbb74ed027
Ricardo Tun <me@ricardotun.mx>
2023-11-29 03:10:22 -0500

    [ruby] boutique inventory: add solution

--- ruby/boutique-inventory/boutique_inventory.rb
+++ ruby/boutique-inventory/boutique_inventory.rb
@@ -4,23 +4,23 @@ class BoutiqueInventory
   end
 
   def item_names
-    raise 'Implement the BoutiqueInventory#item_names method'
+    @items.map { |item| item[:name] }.flatten.sort
   end
 
   def cheap
-    raise 'Implement the BoutiqueInventory#cheap method'
+    @items.select { |item| item[:price] < 30.00 }
   end
 
   def out_of_stock
-    raise 'Implement the BoutiqueInventory#out_of_stock method'
+    @items.select { |item| item[:quantity_by_size].empty? }
   end
 
   def stock_for_item(name)
-    raise 'Implement the BoutiqueInventory#stock_for_item method'
+    @items.find { |item| item[:name].eql? name }[:quantity_by_size]
   end
 
   def total_stock
-    raise 'Implement the BoutiqueInventory#total_stock method'
+    @items.map { |item| item[:quantity_by_size].values.sum }.sum
   end
 
   private
