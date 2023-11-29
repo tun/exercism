eebe89138e5ef020dcb0e4fad50f244f460097d6
Ricardo Tun <me@ricardotun.mx>
2023-11-29 05:40:31 -0500

    [ruby] locomotive engineer: add solution

--- ruby/locomotive-engineer/locomotive_engineer.rb
+++ ruby/locomotive-engineer/locomotive_engineer.rb
@@ -1,17 +1,21 @@
 class LocomotiveEngineer
-  def self.generate_list_of_wagons
-    raise 'Please implement the LocomotiveEngineer.generate_list_of_wagons method'
+  def self.generate_list_of_wagons(*wagons)
+    wagons
   end
 
   def self.fix_list_of_wagons(each_wagons_id, missing_wagons)
-    raise 'Please implement the LocomotiveEngineer.fix_list_of_wagons method'
+    first, second, third, *remainder = each_wagons_id
+
+    [third, *missing_wagons, *remainder, first, second]
   end
 
-  def self.add_missing_stops
-    raise 'Please implement the LocomotiveEngineer.add_missing_stops method'
+  def self.add_missing_stops(route, **stops)
+    route[:stops] = stops.values
+
+    route
   end
 
   def self.extend_route_information(route, more_route_information)
-    raise 'Please implement the LocomotiveEngineer.extend_route_information method'
+    {**route, **more_route_information}
   end
 end
